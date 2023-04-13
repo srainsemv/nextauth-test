25556591f1923d0ba6227f6cd5eecb22e30f82aa
stephen.rains <stephen.rains@electrameccanica.com>
2023-04-13 15:42:28 -0700

    Changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ const options = {
     providers: [
         AzureADProvider({
             clientId: "6b7c9d05-e06a-4e5d-840f-838e04295e1c",
-            clientSecret: "2db8907c-33b1-4726-abbc-f02afcaa995e",
+            clientSecret: "964110eb-855d-4743-ae43-4853300a8505",
             tenantId: "91962bb4-066f-4705-8bbd-ff4dd5ca63f6",
         }),
     ],
